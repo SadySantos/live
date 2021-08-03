af6615b8c7f4ad623f879d39718e4d81939e7661
SadySantos <sadysantos@hotmail.com.br>
2021-08-02 22:15:19 -0300

    $ git commit -m "Primeiro commit"

---
+++ src/main/java/com/dio/live/model/TipoDado.java
@@ -0,0 +1,13 @@
+package com.dio.live.model;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class TipoDado {
+    private long id;
+    private String descricao;
+}
---
+++ src/main/java/com/dio/live/model/Usuario.java
@@ -0,0 +1,30 @@
+package com.dio.live.model;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+public class Usuario {
+    @Id
+    private long id;
+    @ManyToOne
+    private CategotiaUsuario categotiaUsuario;
+    private String nome;
+    @ManyToOne
+    private Empresa empresa;
+    @ManyToOne
+    private NivelAcesso nivelAcesso;
+    @ManyToOne
+    private JornadaTrabalho jornadaTrabalho;
+    private BigDecimal tolerancia;
+    private LocalDateTime inicioJornada;
+    private LocalDateTime finalJornada;
+}
